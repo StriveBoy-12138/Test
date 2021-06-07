bbc4107289c5309933953a4c927ffbe2a18b0fdf
StriveBoy-12138 <1049994365@qq.com>
2021-06-07 16:18:33 +0800

    git xiu

--- src/hello.java
+++ src/hello.java
@@ -4,6 +4,6 @@ public class hello {
         System.out.println("hello");
         System.out.println("world");
         System.out.println("happy");
-        System.out.println("bbb");
+        System.out.println("");
     }
 }
